bf30e9194879757a56206e8d947baa2f9b5e141b
leonshiyan <leonshiyan@gmail.com>
2023-02-19 15:18:06 -0800

    add state variable

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,8 @@
 const winningCombos : number [][] = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],]
 
 /*---------------------------- Variables (state) ----------------------------*/
+let turn : number
+let winner : boolean
+let tie : boolean
+let board : number [] = new Array(9)
 

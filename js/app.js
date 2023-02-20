76d1afa3bb44c22b91dcc536e190e70636aec8cd
leonshiyan <leonshiyan@gmail.com>
2023-02-19 16:20:03 -0800

    code clean up

--- js/app.js
+++ js/app.js
@@ -12,9 +12,9 @@ const messageEl = document.getElementById('message');
 const boardEl = document.querySelector('.board');
 const resetBtnEl = document.querySelector('.reset-button');
 /*----------------------------- Event Listeners -----------------------------*/
-// boardEl.addEventListener('click',handleClick)
-// resetBtnEl.addEventListener('click',init)
-// boardEl.addEventListener('animationend', handleAnimationEnd, {once: false});
+boardEl.addEventListener('click', handleClick, true);
+resetBtnEl.addEventListener('click', init, true);
+boardEl.addEventListener('animationend', handleAnimationEnd, { once: false });
 /*-------------------------------- Functions --------------------------------*/
 function init() {
     board.fill(0);
@@ -22,7 +22,14 @@ function init() {
     winner = false;
     tie = false;
     messageEl.classList.remove('animate__animated', 'animate__flip');
-    //render()
+    render();
+}
+function boardAnimation(event) {
+    boardEl.classList.add('animate__animated', 'animate__headShake');
+}
+function handleAnimationEnd(event) {
+    event.stopPropagation();
+    boardEl.classList.remove('animate__animated', 'animate__headShake');
 }
 function updateBoard() {
     for (let i = 0; i < board.length; i++) {
@@ -37,9 +44,82 @@ function updateBoard() {
         }
     }
 }
-// function render(){
-//   updateBoard()
-//   updateMessage()
-// }
+function updateMessage() {
+    const playerMsg = turn == 1 ? 'X' : 'O';
+    if (!winner && !tie) {
+        messageEl.textContent = `It is player ${playerMsg} turn!`;
+    }
+    else if (!winner && tie) {
+        messageEl.textContent = `It is a tie game!`;
+    }
+    else {
+        messageEl.textContent = `Congratulations, player ${playerMsg} wins!`;
+        messageEl.classList.add('animate__animated', 'animate__flip');
+    }
+}
+function handleClick(event) {
+    let sqIdx;
+    const clickedEl = event.target;
+    const sqId = clickedEl.getAttribute('id');
+    sqIdx = parseInt(sqId.slice(2));
+    if (board[sqIdx] || winner)
+        return;
+    playerMove(sqIdx);
+    setTimeout(() => {
+        computerMove();
+    }, 300);
+}
+function playerMove(sqIdx) {
+    placePiece(sqIdx);
+    checkForTie();
+    checkForWinner();
+    switchPlayerTurn();
+    render();
+}
+function placePiece(sqIdx) {
+    board[sqIdx] = turn;
+}
+function checkForTie() {
+    if (!board.includes(0))
+        tie = true;
+}
+function checkForWinner() {
+    let result;
+    for (const combo of winningCombos) {
+        result = Math.abs(board[combo[0]] + board[combo[1]] + board[combo[2]]);
+        if (result === 3) {
+            winner = true;
+            return;
+        }
+    }
+}
+function switchPlayerTurn() {
+    if (winner) {
+        return;
+    }
+    else {
+        turn *= -1;
+    }
+}
+function computerMove() {
+    if (!winner && !tie) {
+        let computerIdx = chooseEmptySpot();
+        playerMove(computerIdx);
+    }
+}
+function chooseEmptySpot() {
+    let moves = [];
+    for (let index = 0; index < board.length; index++) {
+        if (board[index] === 0)
+            moves.push(index);
+    }
+    //Select a move
+    let move = moves[Math.floor(Math.random() * moves.length)];
+    return move;
+}
+function render() {
+    updateBoard();
+    updateMessage();
+}
 /*-------------------------------- Game init --------------------------------*/
 init();
--- src/app.ts
+++ src/app.ts
@@ -10,14 +10,14 @@ let board : number [] = new Array(9)
 /*------------------------ Cached Element References ------------------------*/
 
 const squareEles: NodeListOf<Element> = document.querySelectorAll('.sqr')
-const messageEl : HTMLElement = document.getElementById('message')!
-const boardEl:Element = document.querySelector('.board')!
-const resetBtnEl:Element = document.querySelector('.reset-button')!
+const messageEl: HTMLElement = document.getElementById('message')!
+const boardEl:HTMLElement = document.querySelector('.board')!
+const resetBtnEl:HTMLElement = document.querySelector('.reset-button')!
 
 /*----------------------------- Event Listeners -----------------------------*/
-// boardEl.addEventListener('click',handleClick)
-// resetBtnEl.addEventListener('click',init)
-// boardEl.addEventListener('animationend', handleAnimationEnd, {once: false});
+boardEl.addEventListener('click',handleClick,true)
+resetBtnEl.addEventListener('click',init,true)
+boardEl.addEventListener('animationend', handleAnimationEnd, {once: false});
 
 /*-------------------------------- Functions --------------------------------*/
 function init(): void{
@@ -26,9 +26,19 @@ function init(): void{
   winner = false 
   tie = false 
   messageEl.classList.remove('animate__animated','animate__flip')
-  //render()
+  render()
 }
-function updateBoard() : void{
+
+function boardAnimation(event: Event): void{
+  boardEl.classList.add('animate__animated','animate__headShake')
+}
+
+function handleAnimationEnd(event: Event): void{
+  event.stopPropagation();
+  boardEl.classList.remove('animate__animated','animate__headShake');
+}
+
+function updateBoard(): void{
   for (let i = 0; i < board.length; i++) {
     if (board[i] === 1) {
       squareEles[i].textContent = 'X';
@@ -40,12 +50,81 @@ function updateBoard() : void{
   }
 }
 
+function updateMessage(): void{
+  const playerMsg: string = turn == 1? 'X':'O'
+  if(!winner && !tie){
+    messageEl.textContent = `It is player ${playerMsg} turn!`
+  }else if(!winner && tie){
+    messageEl.textContent = `It is a tie game!`
+  }else {
+    messageEl.textContent = `Congratulations, player ${playerMsg} wins!`
+    messageEl.classList.add('animate__animated','animate__flip')
+  }
+}
 
+function handleClick (event: MouseEvent){
+  let sqIdx: number
+  const clickedEl = event.target as HTMLElement
+  const sqId = clickedEl.getAttribute('id')!
+  sqIdx = parseInt(sqId.slice(2))
+  if (board[sqIdx] || winner) return
+  playerMove(sqIdx)
+  setTimeout(() => {
+    computerMove()
+  }, 300)
+}
+function playerMove(sqIdx: number): void{
+  placePiece(sqIdx)
+  checkForTie()
+  checkForWinner()
+  switchPlayerTurn()
+  render()
+}
+function placePiece(sqIdx: number) : void{
+  board[sqIdx] = turn
+}
+function checkForTie(): void{
+  if(!board.includes(0)) tie = true
+}
 
+function checkForWinner(): void{
+  let result : number
+  for(const combo of winningCombos){
+    result = Math.abs(board[combo[0]] + board[combo[1]] + board[combo[2]])
+    if (result === 3){
+      winner = true
+      return
+    }
+  }
+}
+function switchPlayerTurn(): void{
+  if(winner){
+    return
+  }else{
+    turn *= -1
+  }
+}
 
-// function render(){
-//   updateBoard()
-//   updateMessage()
-// }
+function computerMove(): void{
+  if(!winner && !tie){
+    let computerIdx: number = chooseEmptySpot()
+    playerMove(computerIdx)
+  }
+}
+
+function chooseEmptySpot(): number{
+  let moves:number[] = []
+  for (let index = 0; index < board.length; index++) {
+    if(board[index]=== 0) moves.push(index)
+  }
+  //Select a move
+  let move = moves[Math.floor(Math.random() * moves.length)]
+  return move
+}
+
+function render(){
+  updateBoard()
+  updateMessage()
+}
 /*-------------------------------- Game init --------------------------------*/
 init()
